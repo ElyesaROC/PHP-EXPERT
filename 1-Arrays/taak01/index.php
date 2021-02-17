90a1e41b123aa29ac38104f7870e9cabae61333b
Elyesa_Caglar <elyesa.caglar2002@gmail.com>
2021-02-17 13:27:12 +0100

    arrays taak 1,2 en 3 af

--- 1-Arrays/taak01/index.php
+++ 1-Arrays/taak01/index.php
@@ -1,1 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $medewerkers = [
+        [
+            'voornaam' => 'Willem',
+            'achternaam' => 'van Oranje',
+            'functie'  => 'koning'
+        ],
+        [
+            'voornaam' => 'Donald',
+            'achternaam' => 'Trump',
+            'functie'  => 'president'
+        ]
+
+    ];
+    echo $medewerkers[0]['voornaam'] . " " . $medewerkers[0]['achternaam'] . " is een " . $medewerkers[0]['functie'] . ".<br> ";
+    echo $medewerkers[1]['voornaam'] . " " . $medewerkers[1]['achternaam'] . " is een " . $medewerkers[1]['functie'] . ".<br> ";;
+    ?>
+</body>
+
+</html>
---
+++ 1-Arrays/taak02/index.php
@@ -0,0 +1,32 @@
+<?php
+$medewerkers = [
+    [
+        'voornaam' => 'Willem',
+        'achternaam' => 'van Oranje',
+        'functie' => 'koning'
+    ],
+    [
+        'voornaam' => 'Donald',
+        'achternaam' => 'Trump',
+        'functie' => 'president'
+    ],
+    [
+        'voornaam' => 'Homer',
+        'achternaam' => 'Simpson',
+        'functie' => 'fabrieksarbeider'
+    ],
+    [
+        'voornaam' => 'Johan',
+        'achternaam' => 'Cruyff',
+        'functie' => 'voetbalicoon'
+    ],
+    [
+        'voornaam' => 'Badr',
+        'achternaam' => 'Hari',
+        'functie' => 'Kickboxer'
+    ]
+];
+echo $medewerkers[0]['voornaam'] . " " . $medewerkers[0]['achternaam'] . " is een " . $medewerkers[0]['functie'] . ".<br> ";
+echo $medewerkers[1]['voornaam'] . " " . $medewerkers[1]['achternaam'] . " is een " . $medewerkers[1]['functie'] . ".<br> ";
+echo $medewerkers[2]['voornaam'] . " " . $medewerkers[2]['achternaam'] . " is een " . $medewerkers[2]['functie'] . ".<br> ";
+echo $medewerkers[3]['voornaam'] . " " . $medewerkers[3]['achternaam'] . " is een " . $medewerkers[3]['functie'] . ".<br> ";
